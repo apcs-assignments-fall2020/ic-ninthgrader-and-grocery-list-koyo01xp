100014b23904d5a11ba9603afb851d64c41fb3db
koyo01xp <21kasakawa@athenian.org>
2020-11-03 23:25:59 +0000

    homework

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,30 @@
+import java.util.Arrays;
+
 public class GroceryList {
-    
+    private String[] arr;
+
+    public GroceryList() {
+        this.arr = new String[10]; 
+    }
+
+    public void add(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
+    public void remove(String item) {
+        for (int i = 0;i < arr.length;i++) {
+            if (arr[i] == item){
+                for(int a = i;a<arr.length-i-1;a++){
+                    arr[a] = arr[a+1];
+                }
+            }
+        }
+    }
+    public String toString(){
+         return (Arrays.toString(arr));//add arrayToString! Check the notes
+    }
 }
